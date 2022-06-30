7e56f3867f291dc447326eac07e8713831be6065
Peter Dobsan <pdobsan@gmail.com>
2022-06-30 14:09:08 +0200

    do completions in another way.

---
+++ completions/mailctl.bash
@@ -0,0 +1,14 @@
+_mailctl()
+{
+    local CMDLINE
+    local IFS=$'\n'
+    CMDLINE=(--bash-completion-index $COMP_CWORD)
+
+    for arg in ${COMP_WORDS[@]}; do
+        CMDLINE=(${CMDLINE[@]} --bash-completion-word $arg)
+    done
+
+    COMPREPLY=( $(/usr/bin/mailctl "${CMDLINE[@]}") )
+}
+
+complete -o filenames -F _mailctl mailctl
