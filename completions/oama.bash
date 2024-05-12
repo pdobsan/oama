757dbda1da4dba6ef53cfda284c72e93e4f4425b
Peter Dobsan <pdobsan@gmail.com>
2024-05-12 22:26:59 +0200

    Add completion scripts.

---
+++ completions/oama.bash
@@ -0,0 +1,14 @@
+_oama()
+{
+    local CMDLINE
+    local IFS=$'\n'
+    CMDLINE=(--bash-completion-index $COMP_CWORD)
+
+    for arg in ${COMP_WORDS[@]}; do
+        CMDLINE=(${CMDLINE[@]} --bash-completion-word $arg)
+    done
+
+    COMPREPLY=( $(/usr/bin/oama "${CMDLINE[@]}") )
+}
+
+complete -o filenames -F _oama oama
